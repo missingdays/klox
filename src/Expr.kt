e6cc02af62df76ad552ab4f1c1f98d5226cc8266
Evgeny Bovykin <daysaremissing@gmail.com>
2018-06-01 16:41:02 +0300

    Add ast types generator

---
+++ src/Expr.kt
@@ -0,0 +1,61 @@
+// THIS FILE IS AUTO GENERATED. SEE tool/lox_ast_types.txt
+abstract class Expr {
+  interface Visitor<R> {
+    fun visitBinaryExpr (expr: Binary) : R
+    fun visitGroupingExpr (expr: Grouping) : R
+    fun visitLiteralExpr (expr: Literal) : R
+    fun visitUnaryExpr (expr: Unary) : R
+  }
+
+  class Binary : Expr {
+    val left: Expr
+    val operator: Token
+    val right: Expr
+    constructor(left: Expr, operator: Token, right: Expr) {
+      this.left = left
+      this.operator = operator
+      this.right = right
+    }
+
+    override fun<R> accept(visitor: Visitor<R>) : R { 
+      return visitor.visitBinaryExpr(this)
+    }
+  }
+
+  class Grouping : Expr {
+    val experession: Expr
+    constructor(experession: Expr) {
+      this.experession = experession
+    }
+
+    override fun<R> accept(visitor: Visitor<R>) : R { 
+      return visitor.visitGroupingExpr(this)
+    }
+  }
+
+  class Literal : Expr {
+    val value: Any
+    constructor(value: Any) {
+      this.value = value
+    }
+
+    override fun<R> accept(visitor: Visitor<R>) : R { 
+      return visitor.visitLiteralExpr(this)
+    }
+  }
+
+  class Unary : Expr {
+    val operator: Token
+    val right: Expr
+    constructor(operator: Token, right: Expr) {
+      this.operator = operator
+      this.right = right
+    }
+
+    override fun<R> accept(visitor: Visitor<R>) : R { 
+      return visitor.visitUnaryExpr(this)
+    }
+  }
+
+  abstract fun<R> accept(visitor: Visitor<R>) : R
+}
---
+++ tool/generate_ast.py
@@ -0,0 +1,67 @@
+import sys
+from collections import namedtuple
+
+Type = namedtuple('Type', 'name fields'.split())
+
+def create_ast(baseclass, grammar_file):
+
+	print("// THIS FILE IS AUTO GENERATED. SEE tool/lox_ast_types.txt")
+	
+	print(f"abstract class {baseclass} " + "{")
+
+	types = []
+
+	with open(grammar_file) as f:
+		for line in f:
+			if line.startswith('#'):
+				continue
+
+			row = line.split(":")
+			types.append(Type(name=row[0].strip(), fields=":".join(row[1:]).strip()))
+
+	create_visitor(baseclass, types)
+
+	for _type in types:
+		create_type(name=_type.name, fields=_type.fields, baseclass=baseclass)
+
+	print("  abstract fun<R> accept(visitor: Visitor<R>) : R")
+
+	print("}")
+
+def create_type(name, fields, baseclass):
+	print(f"  class {name} : {baseclass} " + "{") # class declaration
+
+	for field in fields.split(','):                        # fields declaration
+		print(f"    val {field.strip()}")
+
+	print(f"    constructor({fields}) " + "{")                      # constructor
+	for field in fields.split(','):
+		field_name = field.split(":")[0].strip()
+		print(f"      this.{field_name} = {field_name}")
+	print('    }')
+
+	print("")
+	print("    override fun<R> accept(visitor: Visitor<R>) : R { ")
+	print(f"      return visitor.visit{name}{baseclass}(this)")
+	print("    }")
+
+	print('  }')
+	print("")
+
+def create_visitor(baseclass, types):
+	print("  interface Visitor<R> {")
+	for _type in types:
+		print(f"    fun visit{_type.name}{baseclass} ({baseclass.lower()}: {_type.name}) : R")
+	print("  }")
+	print("")
+
+if __name__ == "__main__":
+	argv = sys.argv
+
+	if len(argv) != 3:
+		print("Usage: generate_ast.py [baseclass] [ast types file]")
+
+	baseclass = sys.argv[1]
+	grammar_file = sys.argv[2]
+
+	create_ast(baseclass, grammar_file)
